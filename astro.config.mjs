1b621021f0c11c7622b0d24e0857ae5d03052a00
TADo <dothanhtupt@gmail.com>
2026-03-09 11:12:38 +0700

    finish config astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,31 @@
-import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import cloudflare from '@astrojs/cloudflare';
-
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
-// https://astro.build/config
 export default defineConfig({
-	adapter: cloudflare({
+  site: "https://tado-site.pages.dev",
+  adapter: cloudflare({
     platformProxy: {
       enabled: true
     },
-
     imageService: "cloudflare"
   }),
-
   integrations: [react(), mdx(), sitemap()],
-
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+    shikiConfig: {
+      themes: { light: "github-light", dark: "github-dark" },
+      wrap: true,
+    },
+  },
 });
