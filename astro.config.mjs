f1d56ba2a2e1fa474f0dbfc69fea1a470661e125
TADo <dothanhtupt@gmail.com>
2026-03-09 10:54:11 +0700

    init blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,27 @@
+import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare({
+	adapter: cloudflare({
     platformProxy: {
       enabled: true
     },
 
     imageService: "cloudflare"
-  })
+  }),
+
+  integrations: [react(), mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
